612d67112679b9269eb9ddc0088dadb6201f87e1
cahyaind <cahyaindsi@gmail.com>
2022-04-26 20:11:16 +0700

    add text and button

--- lib/main.dart
+++ lib/main.dart
@@ -4,16 +4,25 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("My App"),
+        home: Scaffold(
+      appBar: AppBar(title: Text("Anonymous Method")),
+      body: Center(
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Text("initeks"),
+        RaisedButton(
+          child: Text("Tekan"),
+          onPressed: null,
         ),
-        body: Center(child: Text("gimana ya")),
-      ),
-    );
+      ])),
+    ));
   }
 }
