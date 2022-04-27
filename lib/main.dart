adf99d1f1c8e3d0300dea1416c13847da79ef921
cahyaind <cahyaindsi@gmail.com>
2022-04-27 16:00:28 +0700

    add TextStyle

--- lib/main.dart
+++ lib/main.dart
@@ -4,31 +4,59 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatefulWidget {
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  String message = "teks ini";
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(title: Text("Anonymous Method")),
-      body: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-        Text(message),
-        RaisedButton(
-          child: Text("Tekan"),
-          onPressed: () {
-            setState(() {
-              message = "Tombol sudah ditekan";
-            });
-          },
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("TextStyle"),
         ),
-      ])),
-    ));
+        body: Center(
+          child: Text(
+            "lorem ipsum",
+            style: TextStyle(
+              fontFamily: "Caveat",
+              fontSize: 30,
+              decoration: TextDecoration.underline,
+              decorationColor: Colors.blue,
+              decorationThickness: 5,
+              decorationStyle: TextDecorationStyle.wavy,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
+
+// IMPLEMENTASI ANONYMOUS METHOD
+
+// class MyApp extends StatefulWidget {
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
+
+// class _MyAppState extends State<MyApp> {
+//   String message = "teks ini";
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         home: Scaffold(
+//       appBar: AppBar(title: Text("Anonymous Method")),
+//       body: Center(
+//           child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+//         Text(message),
+//         RaisedButton(
+//           child: Text("Tekan"),
+//           onPressed: () {
+//             setState(() {
+//               message = "Tombol sudah ditekan";
+//             });
+//           },
+//         ),
+//       ])),
+//     ));
+//   }
+// }
